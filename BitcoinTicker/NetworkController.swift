3c5860de681f59ed91f6dd75b1201850fc2fb3dc
Jordan Nelson <jordanwebbnelson@gmail.com>
2018-01-10 14:03:48 -0700

    Alomofire basic implementation and request

--- BitcoinTicker/NetworkController.swift
+++ BitcoinTicker/NetworkController.swift
@@ -7,14 +7,44 @@
 //
 
 import Foundation
+import Alamofire
 
 class NetworkController {
     
     
+    static let baseURL = URL(string:"https://api.coinmarketcap.com/v1/ticker/")!
     
+    var coins: [Coin] = []
     
-    
-    
+    func getCurrentCoinData(completion:@escaping() -> Void) {
+        
+        var coins:[Coin] = []
+        
+        Alamofire.request(NetworkController.baseURL).responseJSON { response in
+            
+            if let responseDictionaries = response.result.value as? [[String:AnyObject]] {
+                
+                for coinDictionary in responseDictionaries {
+                    if let coin = Coin(json: coinDictionary) {
+                        coins.append(coin)
+                    }
+                }
+                
+                self.coins = coins
+                completion()
+            } else {
+                print("Coin dictionary response failure")
+                completion()
+            }
+            
+            
+            
+            
+            
+        }
+        
+        
+    }
     
 }
 
