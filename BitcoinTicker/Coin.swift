86104cfc9f6975b1d3aa834ddf52dd6a505b28d7
Jordan Nelson <jordanwebbnelson@gmail.com>
2018-01-10 18:25:54 -0700

    Coin Model finalized

--- BitcoinTicker/Coin.swift
+++ BitcoinTicker/Coin.swift
@@ -19,6 +19,10 @@ class Coin {
     private let k24hrVolume = "24h_volume_usd"
     private let kMarketCapUSD = "market_cap_usd"
     private let kAvailableSupply = "available_supply"
+    private let kTotalSupply = "total_supply"
+    private let kPercentHour = "percent_change_1h"
+    private let kPercentDay = "percent_change_24h"
+    private let kPercentWeek = "percent_change_7d"
 
     var id: String
     var name: String
@@ -29,6 +33,10 @@ class Coin {
     var volume24Hr: String
     var marketCapUSD: String
     var availableSupply: String
+    var totalSupply: String
+    var percentChangeHour: String
+    var percentChangeDay: String
+    var percentChangeWeek: String
     
     init?(json:[String:AnyObject]){
         
@@ -36,7 +44,9 @@ class Coin {
         let symbol = json[kSymbol] as? String, let rank = json[kRank] as? String,
         let priceUSD = json[kPriceUSD] as? String, let priceBTC = json[kPriceBTC] as? String,
         let volume24Hr = json[k24hrVolume] as? String, let marketCapUSD = json[kMarketCapUSD] as? String,
-        let availableSupply = json[kAvailableSupply] as? String
+        let availableSupply = json[kAvailableSupply] as? String, let totalSupply = json[kTotalSupply] as? String,
+        let percentChangeHour = json[kPercentHour] as? String, let percentChangeDay = json[kPercentDay] as? String,
+        let percentChangeWeek = json[kPercentWeek] as? String
             else {
                 self.id = ""
                 self.name = ""
@@ -47,6 +57,10 @@ class Coin {
                 self.volume24Hr = ""
                 self.marketCapUSD = ""
                 self.availableSupply = ""
+                self.totalSupply = ""
+                self.percentChangeDay = ""
+                self.percentChangeHour = ""
+                self.percentChangeWeek = ""
             return
         }
         
@@ -59,5 +73,9 @@ class Coin {
         self.volume24Hr = volume24Hr
         self.marketCapUSD = marketCapUSD
         self.availableSupply = availableSupply
+        self.totalSupply = totalSupply
+        self.percentChangeDay = percentChangeDay
+        self.percentChangeHour = percentChangeHour
+        self.percentChangeWeek = percentChangeWeek
     }
 }
